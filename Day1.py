f27d57e8223eace73a636848217ab023e8275c77
KAVINKUMAR T <kavint2007@gmail.com>
2025-08-24 18:22:06 +0530

    first commit

---
+++ Day1.py
@@ -0,0 +1,20 @@
+#User Input/Output
+a=int(input())
+print(a)
+
+#Datatypes
+
+b=int(input('Integer: '))
+print(type(b))
+
+c=float(input('Floating point number: '))
+print(type(c))
+
+d=input('String: ')
+print(type(d))
+
+e=bool(input('Boolean value: '))
+print(type(e))
+
+f=complex(input('complex value: '))
+print(type(f))
